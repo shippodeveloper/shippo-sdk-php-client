fc6e00720a2386e4f89f1be053bc6dd7156a3b38
luuthieu <tronghieu.luu@gmail.com>
2018-11-06 18:40:43 +0700

    Add tests

---
+++ tests/DateUtilTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luuthieu
+ * Date: 11/5/18
+ * Time: 16:37
+ */
+
+
+class DateUtilTest extends \PHPUnit\Framework\TestCase
+{
+    public function testDateFormat() {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $input = '2016-11-10T08:17:23.000Z';
+        $date = \Shippo\Utils\DateTimeUtil::convertDateTime($input);
+        $this->assertEquals('10/11/2016 15:17:23', $date->format('d/m/Y H:i:s'), 'Date after convert not match');
+    }
+}
